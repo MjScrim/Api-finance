c85328d3efb9407129f483e9f45b59d523d7b446
Marcos Andrade <109639621+MjScrim@users.noreply.github.com>
2025-07-22 01:11:33 -0300

    Subindo a aplicação para o GitHub.

---
+++ src/main/java/com/scrim/crm/ScrimApiApplication.java
@@ -0,0 +1,13 @@
+package com.scrim.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrimApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrim/crm/ScrimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrim.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrimApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
